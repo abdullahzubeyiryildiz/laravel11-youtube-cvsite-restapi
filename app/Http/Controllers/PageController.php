52c688e01106666c82dc7037d9eb114b848f2849
Abdullah Zübeyir Yıldız <35402428+abdullahzubeyiryildiz@users.noreply.github.com>
2024-08-25 14:03:14 +0300

    Hizmetler

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,13 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\About;
 use App\Models\Slider;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
          $slider = Slider::where('status',1)->first();
+
+          $categories = Category::where('status',1)->get();
          $about = About::first();
-        return view('frontend.index',compact('slider','about'));
+        return view('frontend.index',compact('slider','about','categories'));
     }
 }
--- resources/views/frontend/component/services.blade.php
+++ resources/views/frontend/component/services.blade.php
@@ -3,12 +3,10 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-10">
+
             <div class="service-header-area">
               <div class="pbmit-heading-subheading">
-                <h4> <img src="assets/img/icons/star3.png" alt="">OUR SERVICE</h4>
-                <h2 class="text-anime-style-3">Best Digital Marketing</h2>
-                <span class="designer text-anime-style-3">graphic Designer</span>
-                <span class="marketer">SERVICE
+                <span class="marketer">Hizmetler
                   <img src="assets/img/elements/elements5.png" alt="" class="elements1" data-aos="fade-left" data-aos-duration="800">
                   <img src="assets/img/elements/elements5.png" alt="" class="elements2" data-aos="fade-right" data-aos-duration="1000">
                   <img src="assets/img/elements/elements5.png" alt="" class="elements3" data-aos="fade-left" data-aos-duration="1100">
@@ -18,66 +16,44 @@
             </div>
 
             <div class="service-boxs-area">
-              <img src="assets/img/icons/star4.png" alt="" class="star2 keyframe5">
-              <div class="service-author-box box1" style="background-image: url(assets/img/bg/polygon9.png); background-position: center; background-repeat: no-repeat; background-size: contain;" data-aos="zoom-out" data-aos-duration="700">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service7.svg" alt="">
-                  </div>
-                  <p>social media</p>
-                </div>
-              </div>
 
-              <div class="service-author-box box2" data-aos="zoom-out" data-aos-duration="800" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
-                <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service8.svg" alt="">
-                  </div>
-                  <p>email marketing</p>
-                </div>
-              </div>
+                @if (!empty($categories) && $categories->count() > 0)
+                @php
+                    $animationTimes = 700;
+                @endphp
+                @foreach ($categories as $key=> $category)
+                    @php
+                        $animationTimes + 100;
+                    @endphp
+                    @if ($loop->first)
 
-              <div class="service-author-box box3" data-aos="zoom-out" data-aos-duration="900" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
-                <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service9.svg" alt="">
-                  </div>
-                  <p>SEO marketing</p>
-                </div>
-              </div>
+                        <img src="assets/img/icons/star4.png" alt="" class="star2 keyframe5">
+                        <div class="service-author-box box{{$loop->iteration}}" style="background-image: url(assets/img/bg/polygon9.png); background-position: center; background-repeat: no-repeat; background-size: contain;" data-aos="zoom-out" data-aos-duration="{{$animationTimes}}">
+                        <div class="content">
+                            <div class="icons">
+                            <img src="{{asset($category->image)}}" alt="">
+                            </div>
+                            <p>{{$category->name}}</p>
+                        </div>
+                        </div>
+                @else
 
-              <div class="service-author-box box4" data-aos="zoom-out" data-aos-duration="1000" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
-                <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service10.svg" alt="">
-                  </div>
-                  <p>ppc marketing</p>
-                </div>
-              </div>
+                        <div class="service-author-box box{{$loop->iteration}}" data-aos="zoom-out" data-aos-duration="{{$animationTimes}}" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
+                            <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
+                            <div class="content">
+                            <div class="icons">
+                                <img src="{{asset($category->image)}}" alt="">
+                            </div>
+                            <p>{{$category->name}}</p>
+                            </div>
+                        </div>
 
-              <div class="service-author-box box5" data-aos="zoom-out" data-aos-duration="1100" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
-                <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service11.svg" alt="">
-                  </div>
-                  <p>video marketing</p>
-                </div>
-              </div>
+                    @endif
 
-              <div class="service-author-box box6" data-aos="zoom-out" data-aos-duration="1200" style="background-image: url(assets/img/bg/polygon8.png); background-position: center; background-repeat: no-repeat; background-size: contain;">
-                <img src="assets/img/bg/polygon9.png" alt="" class="polygon2">
-                <div class="content">
-                  <div class="icons">
-                    <img src="assets/img/icons/service12.svg" alt="">
-                  </div>
-                  <p>graphic design</p>
-                </div>
+    @endforeach
+            @endif
+
               </div>
-            </div>
           </div>
         </div>
       </div>
